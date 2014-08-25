cd160c7a5397bf40d1f695238223ef902d602b97
vr2262 <vr2262@columbia.edu>
2014-08-25 14:36:59 -0400

    insert into survey

--- python_data_dump/dumper.py
+++ python_data_dump/dumper.py
@@ -1,5 +1,17 @@
+import psycopg2 as pg
+import sys
+
+
 def main():
-    pass
+    """Maximal laziness"""
+
+    pword = sys.argv[1]
+    con = ph.connect(dbname='test', host='localhost', user='postgres',
+                     password=pword)
+    cursor = con.cursor()
+    query = "insert into survey (title, survey_owner) values ('health_mopup_new', 'postgres');"
+    cursor.execute()
+    con.commit()
 
 if __name__ == '__main__':
     main()
