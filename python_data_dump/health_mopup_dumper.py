13de887dafc66cb266fe37ca97604e1fc4c90158
Abdi Dahir <abdi.dahir@outlook.com>
2014-09-05 13:58:13 -0400

    Updated data to fit new schema

--- python_data_dump/health_mopup_dumper.py
+++ python_data_dump/health_mopup_dumper.py
@@ -30,13 +30,13 @@ def main():
 
     cursor.execute('delete from survey;')
     cursor.execute('delete from submission;')
-    cursor.execute('delete from question_type;')
+    cursor.execute('delete from type_constraint;')
+    cursor.execute('delete from question_variety;')
     cursor.execute('delete from question;')
     cursor.execute('delete from question_choice;')
     cursor.execute('delete from question_branch;')
     cursor.execute('delete from answer;')
-    cursor.execute('delete from answer_pick_choice;')
-    cursor.execute('delete from answer_checkbox;')
+    cursor.execute('delete from answer_choice;')
     
     query = "insert into survey (title, survey_owner) values ('health_mopup', 'postgres');"
     cursor.execute(query)
@@ -44,10 +44,10 @@ def main():
     cursor.execute("select survey_id from survey where title='health_mopup';")
     survey_id = cursor.fetchone()[0]
 
-    query = "insert into question_type (question_type_name) values ('integer'), ('multiple_choice');"
+    query = "insert into type_constraint (type_constraint_name) values ('integer'), ('multiple_choice');"
     cursor.execute(query)
 
-    query =  "insert into question (title, sequence_number, question_type_name, survey_id) values "
+    query =  "insert into question (title, sequence_number, type_constraint_name, survey_id) values "
     query += "('antenatal_care_yn', 0, 'multiple_choice', '{}'),".format(survey_id)
     query += "('num_doctors_fulltime', 1, 'integer', '{}'),".format(survey_id)
     query += "('num_midwives_fulltime', 2, 'integer', '{}'),".format(survey_id)
@@ -59,10 +59,10 @@ def main():
     cursor.execute("select question_id from question;")
     question_ids = [row[0] for row in cursor.fetchall()]
 
-    query =  "insert into question_choice (choice, choice_number, question_id, question_type_name, question_sequence_number, survey_id) values"
-    query += "('yes', 1, '{}', 'multiple_choice', 0, '{}'),".format(question_ids[0], survey_id)
-    query += "('no',  2, '{}', 'multiple_choice', 0, '{}'),".format(question_ids[0], survey_id)
-    query += "('dk',  3, '{}', 'multiple_choice', 0, '{}');".format(question_ids[0], survey_id)
+    query =  "insert into question_choice (choice, choice_number, question_id, allow_multiple, type_constraint_name, question_sequence_number, survey_id) values"
+    query += "('yes', 1, '{}', false, 'multiple_choice', 0, '{}'),".format(question_ids[0], survey_id)
+    query += "('no',  2, '{}', false, 'multiple_choice', 0, '{}'),".format(question_ids[0], survey_id)
+    query += "('dk',  3, '{}', false, 'multiple_choice', 0, '{}');".format(question_ids[0], survey_id)
     cursor.execute(query)
 
     cursor.execute("select question_choice_id from question_choice;")
@@ -81,9 +81,9 @@ def main():
     cursor.execute("select question_choice_id from question_choice where choice_number=1")
     choice_id = cursor.fetchone()[0]
 
-    query =  "insert into question_branch (question_choice_id, from_question_id, from_question_type, "
-    query += "from_sequence_number, from_survey_id, to_question_id, to_question_type, to_sequence_number, to_survey_id) values "
-    query += "('{}', '{}', 'multiple_choice', 0, '{}', '{}', 'integer', 2, '{}');".format(choice_id, question_ids[0], survey_id, question_ids[2], survey_id)
+    query =  "insert into question_branch (question_choice_id, from_question_id,  from_allow_multiple, from_type_constraint, "
+    query += "from_sequence_number, from_survey_id, to_question_id, to_allow_multiple, to_type_constraint, to_sequence_number, to_survey_id) values "
+    query += "('{}', '{}', false, 'multiple_choice', 0, '{}', '{}', false, 'integer', 2, '{}');".format(choice_id, question_ids[0], survey_id, question_ids[2], survey_id)
     cursor.execute(query)
 
     data = json.load(urllib2.urlopen(args.file_url))
@@ -94,16 +94,16 @@ def main():
         cursor.execute("select submission_id from submission where submitter = 'postgres{}'".format(index))
         submission_id = cursor.fetchone()[0]
         
-        query = "insert into answer (answer_integer, submission_id, question_id, question_type_name, sequence_number, survey_id) values "
-        query += "({}, '{}', '{}', 'integer', {}, '{}'),".format(none_to_zero(val[index]['num_doctors_fulltime'][0]), submission_id, question_ids[1], 1, survey_id)
-        query += "({}, '{}', '{}', 'integer', {}, '{}'),".format(none_to_zero(val[index]['num_midwives_fulltime'][0]), submission_id, question_ids[2], 2, survey_id)
-        query += "({}, '{}', '{}', 'integer', {}, '{}'),".format(none_to_zero(val[index]['num_nurses_fulltime'][0]), submission_id, question_ids[3], 3, survey_id)
-        query += "({}, '{}', '{}', 'integer', {}, '{}'),".format(none_to_zero(val[index]['num_chews_fulltime'][0]), submission_id, question_ids[4], 4, survey_id)
-        query += "({}, '{}', '{}', 'integer', {}, '{}');".format(none_to_zero(val[index]['num_toilets_total'][0]), submission_id, question_ids[5], 5, survey_id)
+        query = "insert into answer (answer_integer, submission_id, question_id, allow_multiple, type_constraint_name, sequence_number, survey_id) values "
+        query += "({}, '{}', '{}', false, 'integer', {}, '{}'),".format(none_to_zero(val[index]['num_doctors_fulltime'][0]), submission_id, question_ids[1], 1, survey_id)
+        query += "({}, '{}', '{}', false, 'integer', {}, '{}'),".format(none_to_zero(val[index]['num_midwives_fulltime'][0]), submission_id, question_ids[2], 2, survey_id)
+        query += "({}, '{}', '{}', false, 'integer', {}, '{}'),".format(none_to_zero(val[index]['num_nurses_fulltime'][0]), submission_id, question_ids[3], 3, survey_id)
+        query += "({}, '{}', '{}', false, 'integer', {}, '{}'),".format(none_to_zero(val[index]['num_chews_fulltime'][0]), submission_id, question_ids[4], 4, survey_id)
+        query += "({}, '{}', '{}', false, 'integer', {}, '{}');".format(none_to_zero(val[index]['num_toilets_total'][0]), submission_id, question_ids[5], 5, survey_id)
         cursor.execute(query)
         
-        query =  "insert into answer_pick_choice (question_choice_id, question_id, question_type_name, sequence_number, survey_id, submission_id) values "
-        query += "('{}', '{}', 'multiple_choice', 0, '{}', '{}');".format(get_choice_id(val[index]['antenatal_care_yn'][0]), question_ids[0], survey_id, submission_id)
+        query =  "insert into answer_choice (question_choice_id, question_id, allow_multiple, type_constraint_name, sequence_number, survey_id, submission_id) values "
+        query += "('{}', '{}', false, 'multiple_choice', 0, '{}', '{}');".format(get_choice_id(val[index]['antenatal_care_yn'][0]), question_ids[0], survey_id, submission_id)
         cursor.execute(query)
 
     con.commit()
