57d7a9c3baf682018708f69b7063269bc0ee1381
Denis Papathanasiou <dp2704@columbia.edu>
2014-10-08 12:05:37 -0400

    A custom logger, controlled by settings/local_settings

---
+++ settings.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+"""
+This file defines configuration settings for 
+running the server software.
+
+"""
+
+from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
+
+
+# 
+# General
+
+LOG_LEVEL = ERROR # for testing, use DEBUG in a local_settings.py file instead
+
+
+#
+# Testing
+
+# Allow overrides from a local config file,
+# to enable test environments using the
+# solar simulator and proxies for other devices
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
+
---
+++ utils/logger.py
@@ -0,0 +1,24 @@
+
+"""
+Create a custom logger object for use 
+by the rest of the python logic
+
+"""
+
+import logging
+from settings import LOG_LEVEL
+
+def setup_custom_logger(name, level=LOG_LEVEL):
+    """Generate the logger object, define the output format,
+    and return it to the modules which will use it"""
+
+    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
