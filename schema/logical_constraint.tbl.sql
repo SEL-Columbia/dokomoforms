564b83bbb7fd3ec4b26df9b8aa4e4bca8a4dc52a
vr2262 <vr2262@columbia.edu>
2014-10-09 15:21:05 -0400

    A message for the future

--- schema/logical_constraint.tbl.sql
+++ schema/logical_constraint.tbl.sql
@@ -6,6 +6,9 @@ CREATE TABLE logical_constraint
 (
   logical_constraint_id   uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   logical_constraint_name text UNIQUE NOT NULL,
+  -- After discussion with Chris, maybe there should be a json field after all.
+  -- It would make things much easier on the server side. After all, the
+  -- database doesn't actually care.
 
   logical_constraint_last_update_time timestamp with time zone NOT NULL
                                       DEFAULT now()
