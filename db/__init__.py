ff9d6ec282bf45dd57464a10fefaf926cfc31739
vr2262 <vr2262@columbia.edu>
2014-10-10 09:56:42 -0400

    Documentation update

--- db/__init__.py
+++ db/__init__.py
@@ -1,12 +1,24 @@
 """Set up database access."""
 from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
 
 from settings import CONNECTION_STRING
 
 
 engine = create_engine(CONNECTION_STRING, convert_unicode=True)
 
-def set_testing_engine(testing_engine):
-    """This may or may not work."""
+
+def set_testing_engine(testing_engine: Engine):
+    """
+    Use this function to set an alternate testing engine (like SQLite):
+
+    import db
+
+    from sqlalchemy import create_engine
+
+    db.set_engine(create_engine(CONNECTION_STRING, convert_unicode=True))
+
+    :param testing_engine: A SQLAlchemy engine
+    """
     global engine
     engine = testing_engine
