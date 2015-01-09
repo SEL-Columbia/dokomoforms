30e4e35e1fcf6663a0e2ff1ffea20b930271a48d
Viktor Roytman <vr2262@columbia.edu>
2015-01-09 14:42:24 -0500

    Tornado provides current_user for you mango

--- pages/util/ui.py
+++ pages/util/ui.py
@@ -1,21 +1,17 @@
 """ UI utility methods for templates """
 
-import tornado.web
-import api.survey
 import os.path
+
+import api.survey
+
 
 """ handler is implictly supplied somehow? """
 
+
 def get_survey_info(handler, user, count):
-        return [(survey["survey_id"], survey["title"])
-                for survey in api.survey.get_all(user)[:count]]
+    return [(survey["survey_id"], survey["title"])
+            for survey in api.survey.get_all(user)[:count]]
 
-def get_current_user(handler):
-    current_user = ""
-    if handler.current_user:
-        current_user = handler.current_user.decode('utf-8')
-
-    return current_user
 
 def get_uri(handler, path=""):
     return os.path.join(handler.request.uri, path)
