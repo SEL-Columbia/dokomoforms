32792a80441d812074b44c82ca4c467dfe7a1365
Abdi Dahir <abdi.dahir@outlook.com>
2015-02-03 15:14:25 -0500

    added more survey tests

--- tests/frontend_tests.js
+++ tests/frontend_tests.js
@@ -3,35 +3,164 @@ var should = require('should');
 var request = require('supertest');
 var assert = require('assert');
 
+describe('App and Survey Init Tests', function(done) {
+    // globals
+    var window;
+    var raw_survey;
 
-// globals
-var window;
-var survey = require('./fixtures/survey.json');
-
-describe('Next Question Tests', function(done) {
     before(function(done) {
-        jsdom.env('./tests/widgets.html',  ['../static/lib.js', 'classList_shim.js', '../static/app.js'], 
+        done();
+    });
+
+    beforeEach(function(done) {
+        raw_survey = require('./fixtures/survey.json');
+        jsdom.env('./tests/widgets.html',  
+            ['../static/lib.js', 'classList_shim.js', '../static/app.js'], 
             function(error, win) {
                 if (error) throw (error);
             
                 window = win;
+                window.localStorage = {};
                 done();
             });
-    });
 
-    beforeEach(function(done) {
-        window.localStorage = {};
-        window.App.init(survey)
-        done();
     });
 
     afterEach(function(done) {
+        raw_survey = null;
+        window.close();
+        window = null;
         done();
     });
 
-    describe('Simple Move Question', function() {
-        it('should move from question 0 to 1 when next is clicked', function(done) {
+    it('should init app and survey juust fine', 
+        function(done) {
+            window.App.init(raw_survey);
+            raw_survey.metadata.location.should.match(window.App.start_loc);
+            should(window.App.survey).be.ok;
 
+            var survey = window.App.survey;
+            should(survey.id).match(raw_survey.survey_id);
+            should(survey.questions).match(raw_survey.questions);
+            should(survey.metadata).match(raw_survey.metadata);
+            done();
+        });
+
+    it('should order the survey questions just fine', 
+        function(done) {
+            window.App.init(raw_survey);
+            var survey = window.App.survey;
+            
+            var question = survey.current_question;
+            var sequence_num = 1;
+
+            var len = survey.questions.length;
+
+            while(question) {
+                sequence_num.should.match(question.sequence_number);
+                sequence_num++;
+                question = question.next;
+            }
+
+            sequence_num.should.match(len + 1);
+            done();
+        });
+    
+});
+
+
+describe('Survey tests', function(done) {
+    // globals
+    var window;
+    var raw_survey;
+
+    before(function(done) {
+        done();
+    });
+
+    beforeEach(function(done) {
+        raw_survey = require('./fixtures/survey.json');
+        jsdom.env('./tests/widgets.html',  
+            ['../static/lib.js', 'classList_shim.js', '../static/app.js'], 
+            function(error, win) {
+                if (error) throw (error);
+            
+                window = win;
+                window.localStorage = {};
+                done();
+            });
+
+    });
+
+    afterEach(function(done) {
+        raw_survey = null;
+        window.close();
+        window = null;
+        done();
+    });
+
+    it('should init app and survey juust fine', 
+        function(done) {
+            var questions = [
+                {
+                    question_to_sequence_number: 2,
+                    type_constraint_name: "text",
+                    sequence_number: 1
+                },
+                {
+                    question_to_sequence_number: 2,
+                    type_constraint_name: "text",
+                    sequence_number: 3
+                },
+                {
+                    question_to_sequence_number: 3,
+                    type_constraint_name: "text",
+                    sequence_number: -1
+                },
+            ];
+
+            var survey = new window.Survey("id", questions, {});
+
+            done();
+
+        });
+    
+});
+
+
+describe('Next Question Tests', function(done) {
+    // globals
+    var window;
+    var raw_survey;
+
+    before(function(done) {
+        done();
+    });
+
+    beforeEach(function(done) {
+        raw_survey = require('./fixtures/survey.json');
+        jsdom.env('./tests/widgets.html',  
+            ['../static/lib.js', 'classList_shim.js', '../static/app.js'], 
+            function(error, win) {
+                if (error) throw (error);
+            
+                window = win;
+                window.localStorage = {};
+                window.App.init(raw_survey)
+                done();
+            });
+
+    });
+
+    afterEach(function(done) {
+        raw_survey = null;
+        window.close();
+        window = null;
+        done();
+    });
+
+    it('should move from question 0 to 1 when next is clicked', 
+        function(done) {
             var survey = window.App.survey;
             var questions = survey.questions;
 
@@ -48,7 +177,8 @@ describe('Next Question Tests', function(done) {
             done();
         });
 
-        it('should move from last question to submit page when next is clicked', function(done) {
+    it('should move from last question to submit page when next is clicked',
+        function(done) {
             var survey = window.App.survey;
             var questions = survey.questions;
 
@@ -61,13 +191,14 @@ describe('Next Question Tests', function(done) {
 
             // current question should remain the same on submitters page
             last_question.should.equal(survey.current_question);
-            window.$(".question__title").html().trim().should.equal("That's it, you're finished!");
+            window.$(".question__title").html().trim()
+                .should.equal("That's it, you're finished!");
 
             done();
         });
 
-        it('should move from question 0 to nowhere when prev is clicked', function(done) {
-
+    it('should move from question 0 to nowhere when prev is clicked', 
+        function(done) {
             var survey = window.App.survey;
             var questions = survey.questions;
 
@@ -83,7 +214,8 @@ describe('Next Question Tests', function(done) {
             done();
         });
 
-        it('should move from submit page to current question when prev is clicked', function(done) {
+    it('should move from submit page to current question when prev is clicked', 
+        function(done) {
             var survey = window.App.survey;
             var questions = survey.questions;
             
@@ -101,6 +233,4 @@ describe('Next Question Tests', function(done) {
             window.$(".question__title").html().trim().should.match(title);
             done();
         });
-    });
 });
-
