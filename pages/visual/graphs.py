9dbe9a92384006963353a9b154e40ef3470ab2b9
Viktor Roytman <vr2262@columbia.edu>
2015-02-03 16:04:21 -0500

    Clicking the marker displays the submission

--- pages/visual/graphs.py
+++ pages/visual/graphs.py
@@ -1,9 +1,13 @@
+from sqlalchemy.engine import ResultProxy
+from tornado.escape import json_encode
 import tornado.web
 from db.answer import get_answers_for_question, get_geo_json
 
 from pages.util.base import BaseHandler
 
 from pages.util.visual import user_owns_question
+
+import api.submission
 
 
 class GraphVisualizationHandler(BaseHandler):
@@ -14,11 +18,18 @@ class GraphVisualizationHandler(BaseHandler):
 
 
 class MapVisualizationHandler(BaseHandler):
+    def _get_map_data(self, raw_answers: ResultProxy):
+        for raw_answer in raw_answers:
+            coord = get_geo_json(raw_answer)['coordinates']
+            sub_id = raw_answer.submission_id
+            subs = api.submission.get_one(sub_id, email=self.current_user)
+            yield [coord[0], coord[1], json_encode(subs['result'])]
+
+
     @tornado.web.authenticated
     @user_owns_question
     def get(self, question_id: str):
-        raw_answers = get_answers_for_question(question_id).fetchall()
-        answers = list(map(get_geo_json, raw_answers))
-        coordinates = list(map(lambda x: x['coordinates'], answers))
+        answers = get_answers_for_question(question_id).fetchall()
+        coords_and_subs = list(self._get_map_data(answers))
         self.render('view-map-visualization.html', question_id=question_id,
-                    coordinates=coordinates)
+                    coordinates_and_submissions=coords_and_subs)
--- static/viktors-cool-map.js
+++ static/viktors-cool-map.js
@@ -1,4 +1,5 @@
-function displayViktorPoints(coordinates) {
+function displayViktorPoints(coords_and_submissions) {
+
     var map = L.map('viktormap', {
         //center: [40.8138912, -73.9624327],
         dragging: true,
@@ -10,13 +11,13 @@ function displayViktorPoints(coordinates) {
 
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
 
-    var sw_lat = parseFloat(coordinates[0][1]);
-    var sw_lng = parseFloat(coordinates[0][0]);
-    var ne_lat = parseFloat(coordinates[0][1]);
-    var ne_lng = parseFloat(coordinates[0][0]);
+    var sw_lat = parseFloat(coords_and_submissions[0][1]);
+    var sw_lng = parseFloat(coords_and_submissions[0][0]);
+    var ne_lat = parseFloat(coords_and_submissions[0][1]);
+    var ne_lng = parseFloat(coords_and_submissions[0][0]);
 
-    for (i = 0; i < coordinates.length; i++) {
-        var location = coordinates[i];
+    for (i = 0; i < coords_and_submissions.length; i++) {
+        var location = coords_and_submissions[i];
         var lng = parseFloat(location[0]);
         if (lng < sw_lng) {
             sw_lng = lng;
@@ -33,7 +34,18 @@ function displayViktorPoints(coordinates) {
         var marker = new L.marker([lat, lng], {
             riseOnHover: true
         });
-        marker.options.icon = new L.icon({iconUrl: "/static/img/icons/normal_water.png"});
+        marker.options.icon = new L.icon({iconUrl: "/static/img/icons/selected-point.png", iconAnchor: [15, 48]});
+
+        var answers = JSON.parse(coords_and_submissions[i][2]).answers;
+        var submission_text = "<ul>";
+        for(j=0; j < answers.length; j++){
+            var ans = answers[j];
+            submission_text += "<li><strong>" + ans.sequence_number + ". " + ans.question_title + "</strong><br />" + ans.answer +"</li>";
+        }
+        submission_text += "</ul";
+
+        marker.bindPopup(submission_text);
+
         marker.addTo(map);
     }
 
