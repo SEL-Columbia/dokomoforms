cefc97898a84e04261e435b444a06e5fa4bd5e39
Viktor Roytman <vr2262@columbia.edu>
2015-02-12 10:09:48 -0500

    Test run now reports result on saucelabs

--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -1,11 +1,14 @@
 """Front-end tests"""
+import base64
+import functools
+from http.client import HTTPConnection
+import json
 import os
 import unittest
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
-
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -27,10 +30,22 @@ def go_to_new_window(driver: WebDriver):
         driver.switch_to.window(handle)
 
 
+def report_success_status(method):
+    @functools.wraps(method)
+    def set_passed(self, *args, **kwargs):
+        result = method(self, *args, **kwargs)
+        self.passed = True
+        return result
+
+    return set_passed
+
+
 class DriverTest(unittest.TestCase):
     def setUp(self):
-        username = os.environ.get('SAUCE_USERNAME', SAUCE_USERNAME)
-        access_key = os.environ.get('SAUCE_ACCESS_KEY', SAUCE_ACCESS_KEY)
+        self.passed = False
+
+        self.username = os.environ.get('SAUCE_USERNAME', SAUCE_USERNAME)
+        self.access_key = os.environ.get('SAUCE_ACCESS_KEY', SAUCE_ACCESS_KEY)
         browser_config = os.environ.get('BROWSER', DEFAULT_BROWSER)
         browser_name, version, platform, *other = browser_config.split(':')
         caps = {'browserName': browser_name,
@@ -45,18 +60,29 @@ class DriverTest(unittest.TestCase):
             caps['build'] = os.environ['TRAVIS_BUILD_NUMBER']
             caps['tags'] = [os.environ['TRAVIS_PYTHON_VERSION'], 'CI']
 
-        hub_url = '{}:{}@localhost:4445'.format(username, access_key)
+        hub_url = '{}:{}@localhost:4445'.format(self.username, self.access_key)
         cmd_executor = 'http://{}/wd/hub'.format(hub_url)
         self.drv = webdriver.Remote(desired_capabilities=caps,
                                     command_executor=cmd_executor)
         self.drv.implicitly_wait(10)
 
+    def _set_sauce_status(self):
+        credential_s = ':'.join([self.username, self.access_key])
+        credential_b = credential_s.encode('utf-8')
+        auth = base64.encodebytes(credential_b)[:-1]
+        body = json.dumps({'passed': self.passed})
+        connection = HTTPConnection('saucelabs.com')
+        path = '/rest/v1/{}/jobs/{}'.format(self.username, self.drv.session_id)
+        headers = {'Authorization': 'Basic ' + auth.decode('utf-8')}
+        connection.request('PUT', path, body, headers=headers)
 
     def tearDown(self):
         submission_table.delete().execute()
         auth_user_table.delete().where(
             auth_user_table.c.email == 'test@mockmyid.com').execute()
         self.drv.quit()
+
+        self._set_sauce_status()
 
 
 # This test doesn't play nice with Sauce Labs, and I'm confident that
@@ -74,19 +100,20 @@ class DriverTest(unittest.TestCase):
 # @id="authentication_email"]')
 # eml.send_keys('test@mockmyid.com', Keys.RETURN)
 # self.drv.switch_to.window(self.drv.window_handles[0])
-#         load = EC.presence_of_element_located((By.ID, 'logout'))
-#         WebDriverWait(self.drv, 10).until(load)
+# load = EC.presence_of_element_located((By.ID, 'logout'))
+# WebDriverWait(self.drv, 10).until(load)
 #
-#         self.assertIn('Welcome: test@mockmyid.com', self.drv.page_source)
+# self.assertIn('Welcome: test@mockmyid.com', self.drv.page_source)
 #
-#         self.drv.find_element_by_id('logout').click()
-#         load2 = EC.presence_of_element_located((By.ID, 'login'))
-#         WebDriverWait(self.drv, 2).until(load2)
+# self.drv.find_element_by_id('logout').click()
+# load2 = EC.presence_of_element_located((By.ID, 'login'))
+# WebDriverWait(self.drv, 2).until(load2)
 #
-#         self.assertIn('Welcome to <em>Dokomo</em>', self.drv.page_source)
+# self.assertIn('Welcome to <em>Dokomo</em>', self.drv.page_source)
 
 
 class SubmissionTest(DriverTest):
+    @report_success_status
     def testSubmitSuccessfully(self):
         # Log in
         self.drv.get(base + '/debug/login/test_email')
