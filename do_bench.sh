e1dc6b269ba819ef52514136f97e89f0ba1b5b7b
Abdi Dahir <abdi.dahir@outlook.com>
2015-02-19 17:30:03 -0500

    scripts to speed things up

---
+++ do_bench.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#config this how you want 
+# assumes:
+# 100 surveys
+# create endpoint is desired
+# you have survey.json present in benchmark
+
+sh clean_slate.sh > /dev/null 
+
+if [ -z "`awk /TEST_USER/ local_settings.py`" ]; 
+then 
+    echo 'TEST_USER = "poop"' >> local_settings.py; 
+else
+    sed -i 's/TEST_USER.*/TEST_USER="poop"/' local_settings.py
+fi
+
+python -m cProfile -o /tmp/prof benchmark/benchmark.py --path=/api/surveys/create --post_file=benchmark/survey.json -n=100
