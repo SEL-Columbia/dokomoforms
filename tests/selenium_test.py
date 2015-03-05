b87dfeab1674175f9cef70a8f854ef5800adbb0b
Viktor Roytman <vr2262@columbia.edu>
2015-03-05 15:47:14 -0500

    Add test class name to selenium run name

--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -67,8 +67,10 @@ class DriverTest(unittest.TestCase):
             caps['tunnel-identifier'] = os.environ['TRAVIS_JOB_NUMBER']
             caps['build'] = os.environ['TRAVIS_BUILD_NUMBER']
             caps['tags'] = [os.environ['TRAVIS_PYTHON_VERSION'], 'CI']
-            caps['name'] = ' -- '.join([os.environ['TRAVIS_BUILD_NUMBER'],
-                                        self.browser_config])
+            caps['name'] = ' -- '.join([
+                os.environ['TRAVIS_BUILD_NUMBER'],
+                self.browser_config,
+                self.__class__.__name__])
         else:
             caps['name'] = 'Manual run -- ' + self.browser_config
         hub_url = '{}:{}@localhost:4445'.format(self.username, self.access_key)
