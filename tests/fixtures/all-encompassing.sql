0d0e76a0c3790a690e6e78d926b66d6e8b8f4dc2
Abdi Dahir <abdi.dahir@outlook.com>
2015-03-31 15:38:43 -0400

    fixing metadata on splash

--- tests/fixtures/all-encompassing.sql
+++ tests/fixtures/all-encompassing.sql
@@ -11,7 +11,8 @@ VALUES ('test_email')
 RETURNING auth_user_id INTO the_auth_user_id;
 
 INSERT INTO survey (survey_title, auth_user_id, survey_metadata)
-VALUES ('test_title', the_auth_user_id, '{"location": {"lon": 5.118915, "lat": 7.353078}}')
+VALUES ('test_title', the_auth_user_id, '{"author": "Abdizzle", "organization": "SEL", "location": [5.118915, 7.353078]}')
+
 RETURNING survey_id INTO the_survey_id;
 
 INSERT INTO question (survey_id, sequence_number, question_title,
@@ -92,7 +93,7 @@ VALUES ('a.dahir7@gmail.com')
 RETURNING auth_user_id INTO the_auth_user_id;
 
 INSERT INTO survey (survey_title, auth_user_id, survey_metadata)
-VALUES ('test_title2', the_auth_user_id, '{"location": {"lon": 5.118915, "lat": 7.353078}}')
+VALUES ('test_title2', the_auth_user_id, '{"author": "Abdizzle", "organization": "SEL", "location": [5.118915, 7.353078]}')
 RETURNING survey_id INTO the_survey_id;
 
 INSERT INTO question (survey_id, sequence_number, logic, question_title,
@@ -204,7 +205,7 @@ VALUES (the_survey_id, 1, 'rate me', 'integer', True, 2),
        (the_survey_id, 3, 'im gonan ask you out anyway', 'note', False, -1);
 
 INSERT INTO survey (survey_title, auth_user_id, survey_metadata)
-VALUES ('my favourite number', the_auth_user_id, '{"location": [5.118915, 7.353078]}')
+VALUES ('my favourite number', the_auth_user_id, '{"author": "Abdizzle", "organization": "SEL", "location": [5.118915, 7.353078]}')
 RETURNING survey_id INTO the_survey_id;
 
 INSERT INTO question (survey_id, sequence_number, question_title,
@@ -223,8 +224,8 @@ VALUES (the_survey_id, 1, 'rat3e life', 'integer', False, 2),
        (the_survey_id, 3, 'it wasnt', 'note', False, -1),
        (the_survey_id, 2, 'was it really worth it', 'text', True, 3);
 
-INSERT INTO survey(survey_title, auth_user_id)
-VALUES ('days of the week', the_auth_user_id)
+INSERT INTO survey(survey_title, auth_user_id, survey_metadata)
+VALUES ('days of the week', the_auth_user_id, '{"author": "Abdizzle", "location": [5.118915, 7.353078]}')
 RETURNING survey_id INTO the_survey_id;
 
 INSERT INTO question (survey_id, sequence_number, question_title,
