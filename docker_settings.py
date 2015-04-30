a328cf0891c679693205a61c3aa86cf94f62d82e
Viktor Roytman <vr2262@columbia.edu>
2015-04-30 16:14:10 -0400

    PEP8

--- docker_settings.py
+++ docker_settings.py
@@ -9,9 +9,9 @@ DB_NAME = 'doko'
 
 pg2_string = 'postgresql+psycopg2://{}:{}@{}:{}/{}'
 CONNECTION_STRING = pg2_string.format(
-        DB_USER,
-        DB_PASSWORD,
-        DB_HOST,
-        DB_PORT,
-        DB_NAME
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_PORT,
+    DB_NAME
 )
