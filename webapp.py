001a989427e9d8ff3259c3a7aca7150d2908f361
Viktor Roytman <vr2262@columbia.edu>
2015-05-18 10:10:51 -0400

    Clean up webapp.py a little, mostly checking logic for dropping the schema.

--- webapp.py
+++ webapp.py
@@ -7,6 +7,8 @@ ensure that the proper tables and extensions exist in the specified schema.
 
 import logging
 import os.path
+import sys
+import textwrap
 import tornado.ioloop
 import tornado.locale
 import tornado.web
@@ -20,9 +22,17 @@ from sqlalchemy.orm import sessionmaker
 
 
 _pwd = os.path.dirname(__file__)
-header_color = '\033[1m'
-msg_color = '\033[92m'
-end_color = '\033[0m'
+begin_bold = '\033[1m'
+begin_green = '\033[92m'
+end_modify = '\033[0m'
+
+
+def bold(text):
+    return begin_bold + text + end_modify
+
+
+def colorize(text):
+    return begin_green + text + end_modify
 
 
 class Application(tornado.web.Application):
@@ -43,9 +53,9 @@ class Application(tornado.web.Application):
             'debug': options.debug,
         }
         super().__init__(urls, **settings)
-        # TODO: configurable?
         self.engine = create_engine()
         if options.kill:
+            logging.info('Dropping schema {}.'.format(options.schema))
             self.engine.execute(
                 DDL('DROP SCHEMA IF EXISTS {} CASCADE'.format(options.schema))
             )
@@ -54,25 +64,39 @@ class Application(tornado.web.Application):
         Base.metadata.create_all(self.engine)
 
 
+def ensure_that_user_wants_to_drop_schema():
+    answer = input(textwrap.fill(
+        'Do you really want to drop the schema {schema}? Doing so will {erase}'
+        ' all the data {permanently} y/n (default n)'.format(
+            schema=options.schema,
+            erase=bold('ERASE'),
+            permanently=bold('PERMANENTLY!!!'),
+        )
+    ) + ' ')
+    if answer.lower().startswith('y'):
+        schema_check = input('Enter the exact name of the schema to drop: ')
+        if schema_check == options.schema:
+            return
+    print('Not dropping the schema. Exiting...')
+    sys.exit()
+
+
 def main():
     if options.cookie_secret is None:  # TODO: move into a function
         print('You must set cookie_secret in local_config.py!')
         return
-    if options.kill:  # TODO: move this into a "warning" function
-        answer = input(
-            'Do you really want to drop the schema {}? y/n '
-            '(Default n) '.format(options.schema)
-        )
-        if not answer.lower().startswith('y'):
-            print('Not dropping the schema. Exiting.')
-            return
+    if options.kill:
+        ensure_that_user_wants_to_drop_schema()
     http_server = tornado.httpserver.HTTPServer(Application())
     tornado.locale.load_gettext_translations(
         os.path.join(_pwd, 'locale'), 'dokomoforms'
     )
     logging.info(
-        '{}Dokomo Forms: {}{}starting server on port {}{}'.format(
-            header_color, end_color, msg_color, options.port, end_color
+        '{dokomo}{starting}'.format(
+            dokomo=bold('Dokomo Forms: '),
+            starting=colorize(
+                'starting server on port {}'.format(options.port)
+            ),
         )
     )
     http_server.listen(options.port)
