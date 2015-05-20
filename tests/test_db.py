def60a13eaa91fda66a92d19ab8b881d8ddbab29
Viktor Roytman <vr2262@columbia.edu>
2015-05-20 09:56:16 -0400

    doc

--- tests/test_db.py
+++ tests/test_db.py
@@ -8,4 +8,5 @@ utils = (setUpModule, tearDownModule)
 
 class TestSchema(unittest.TestCase):
     def test_schema_set_properly(self):
+        """It took a lot of effort to make Tornado use a testing schema."""
         self.assertEqual(Base.metadata.schema, 'doko_test')
