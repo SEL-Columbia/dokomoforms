0527dbc985a0a61674403b2a004b5648b57f52d9
Viktor Roytman <vr2262@columbia.edu>
2015-07-12 19:07:38 -0400

    oops

--- dokomoforms/api/surveys.py
+++ dokomoforms/api/surveys.py
@@ -136,7 +136,7 @@ class SurveyResource(BaseResource):
                 func.count(Submission.id),
             )
             .select_from(Submission)
-             .join(Survey)
+            .join(Survey)
             # TODO: ask @jmwohl what this line is supposed to do
             # .filter(User.id == self.current_user_model.id)
             .filter(Submission.survey_id == survey_id)
