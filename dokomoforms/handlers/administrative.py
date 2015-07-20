03c5091473df698f67409df9a2c466aead037e42
Viktor Roytman <vr2262@columbia.edu>
2015-07-20 12:40:21 -0400

    Started migrating existing page resources (templates and static files).

--- dokomoforms/handlers/administrative.py
+++ dokomoforms/handlers/administrative.py
@@ -5,15 +5,39 @@ import tornado.gen
 import tornado.httpclient
 
 from dokomoforms.handlers.util import BaseHandler
+from dokomoforms.models import Survey, Submission
 
 
 class Index(BaseHandler):
 
     """The root URL."""
 
-    def get(self):
+    def get(self, msg=''):
         """GET /."""
-        self.render('administrative_index.html')
+        surveys = None
+        recent_submissions=None
+        if self.current_user:
+            surveys = (
+                self.session
+                .query(Survey)
+                .filter_by(creator_id=self.current_user_model.id)
+                .order_by('created_on DESC')
+                .limit(10)
+            )
+            recent_submissions = (
+                self.session
+                .query(Submission)
+                .join(Survey)
+                .filter_by(creator_id=self.current_user_model.id)
+                .order_by('save time DESC')
+                .limit(5)
+            )
+        self.render(
+            'administrative_index.html',
+            message=msg,
+            surveys=surveys,
+            recent_submissions=recent_submissions,
+        )
 
 
 class NotFound(BaseHandler):
