52e5393458f5570e9f00e8072c516560ade7d1ef
Viktor Roytman <vr2262@columbia.edu>
2015-08-10 17:23:15 -0400

    should make things go more smoothly

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -203,6 +203,7 @@ class TestAuth(DriverTest):
             .find_element_by_id('authentication_email')
             .send_keys('test@mockmyid.com', Keys.RETURN)
         )
+        self.drv.implicitly_wait(5)
         self.switch_window(go_back=True)
         self.wait_for_element('UserDropdown', timeout=10)
         self.assertIn('Recent Submissions', self.drv.page_source)
