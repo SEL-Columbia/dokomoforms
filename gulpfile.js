ce8c4e720521fec6037fb7d4847e6b1647e3352a
Jonathan Wohl <jon@jonwohl.com>
2015-09-04 12:13:55 -0400

    fixed react npm loading issue

--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,7 @@ var path = {
         node_modules_path + '/jquery/dist/jquery.js',
         node_modules_path + '/bootstrap/dist/js/bootstrap.js',
         node_modules_path + '/lodash/lodash.js',
-        node_modules_path + '/react/react.js'
+        node_modules_path + '/react/dist/react.js'
     ],
     SURVEY_JS_APP_SRC: survey_src_path + '/js',
     SURVEY_JS_ENTRY_POINT: survey_src_path + '/js/main.js',
