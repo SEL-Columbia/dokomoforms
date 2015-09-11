eea366bbcce77b8c4a24ead31c9929791c4a698f
Viktor Roytman <vr2262@columbia.edu>
2015-09-11 15:55:31 -0400

    Explanatory note

--- tests/prepare_docker_testing_environment.sh
+++ tests/prepare_docker_testing_environment.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
-# For the last line to work (exporting the DISPLAY environment variable)
-# you should source this file rather than just executing it. Of course, you
-# could export it yourself.
+
+# In order to export the DISPLAY environment variable you should source this
+# script rather than just executing it. Of course, you could export it
+# yourself.
+
 set -e
 apt-get update
 apt-get -y install xvfb iceweasel npm nodejs-legacy
 pip install tox
 Xvfb :1 -screen 0 1024x768x16 &>/dev/null  &
 export DISPLAY=:1.0
+printf "====================================================\n"
+printf "If you didn't source this file, execute\n"
+printf "export DISPLAY=:1.0\n\n"
+printf "Now you can run any tox command. Don't use xvfb-run.\n"
+printf "====================================================\n"
