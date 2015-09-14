f17e276cbd592c35d57d88cdcbd5988cd6e0b7bb
Jonathan Wohl <jon@jonwohl.com>
2015-09-14 12:12:20 -0400

    added data to content security policy header

--- dokomoforms/handlers/util.py
+++ dokomoforms/handlers/util.py
@@ -48,7 +48,7 @@ class BaseHandler(tornado.web.RequestHandler):
             "style-src 'self' 'unsafe-inline'"
             " fonts.googleapis.com cdn.leafletjs.com;"
             "font-src 'self' fonts.googleapis.com fonts.gstatic.com;"
-            "img-src 'self' *.tile.openstreetmap.org;"
+            "img-src 'self' *.tile.openstreetmap.org; data "
             "default-src 'self';"
         )
 
