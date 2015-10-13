06008a06cc63236b07b4befeb7397fbc032e0be7
Viktor Roytman <vr2262@columbia.edu>
2015-10-12 22:12:07 -0400

    missing comma

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,7 @@ var path = {
         node_modules_path + '/lz-string/libs/lz-string.min.js',
         node_modules_path + '/pouchdb/dist/pouchdb.min.js',
         node_modules_path + '/pouchdb-upsert/dist/pouchdb.upsert.min.js',
-        node_modules_path + '/node-uuid/uuid.js'
+        node_modules_path + '/node-uuid/uuid.js',
         node_modules_path + '/screenfull/dist/screenfull.js'
     ],
     SURVEY_JS_APP_SRC: survey_src_path + '/js/**/*.js',
