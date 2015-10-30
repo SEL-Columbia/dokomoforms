6d24c4dbe5dda0cb9dc42862f1f599db64ea5c0e
Viktor Roytman <vr2262@columbia.edu>
2015-10-30 15:56:20 -0400

    ...

--- dokomoforms/static/src/admin/js/account-overview.js
+++ dokomoforms/static/src/admin/js/account-overview.js
@@ -67,7 +67,7 @@ var AccountOverview = (function() {
     function loadRecentSubmissions() {
         var limit = 5;
         return $.getJSON('/api/v0/submissions?order_by=save_time:DESC&limit=' + limit +
-            '&fields=id,submission_time,submitter_name,survey_title,answers');
+            '&fields=id,submission_time,submitter_name,survey_title,survey_default_language,answers');
     }
 
     function drawMap(data) {
