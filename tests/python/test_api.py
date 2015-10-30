b2479be097d8f16243aa2d15411d800de1da03fd
Jonathan Wohl <jon@jonwohl.com>
2015-10-30 11:19:49 -0400

    Fixed another API search test

--- tests/python/test_api.py
+++ tests/python/test_api.py
@@ -2085,7 +2085,7 @@ class TestSurveyApi(DokoHTTPTest):
         )
         self.assertEqual(len(survey_dict['surveys']), 6)
         self.assertEqual(survey_dict['total_entries'], 14)
-        self.assertEqual(survey_dict['filtered_entries'], 6)
+        self.assertEqual(survey_dict['filtered_entries'], 7)
 
     def test_list_surveys_with_specific_fields_and_filter_and_limit(self):
         # url to tests
