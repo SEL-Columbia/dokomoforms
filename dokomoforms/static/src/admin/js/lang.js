0b97caa7a73c3036818687676ccb478d1eb51eea
Jonathan Wohl <jon@jonwohl.com>
2015-10-30 13:49:40 -0400

    fixed bug in lang.js, added refresh on default language setting change

--- dokomoforms/static/src/admin/js/lang.js
+++ dokomoforms/static/src/admin/js/lang.js
@@ -2,8 +2,7 @@
 var _ = require('lodash');
 
 module.exports = (function() {
-    var default_lang = window.CURRENT_USER_PREFS ? window.CURRENT_USER_PREFS.default_language : 'English',
-        selected_lang = 'English';
+    var selected_lang = window.CURRENT_USER_PREFS ? window.CURRENT_USER_PREFS.default_language : 'English';
 
     function getAvailableLanguages(lang_obj) {
         if (!_.isPlainObject(lang_obj)) {
@@ -12,24 +11,26 @@ module.exports = (function() {
         return _.keys(lang_obj);
     }
 
-    function parseTranslateable(lang_obj) {
+    function parseTranslateable(lang_obj, default_lang) {
+        if (!_.isPlainObject(lang_obj)) {
+            throw new Error('lang_obj must be a plain object with keys indicating language');
+        }
+
         if (lang_obj[selected_lang]) {
             return lang_obj[selected_lang];
-        } else if(lang_obj[default_lang]) {
+        } else if(default_lang && lang_obj[default_lang]) {
+            // if a default lang is specified, fall back that
             return lang_obj[default_lang];
         }
+        // finally, fall back to first available language
+        return lang_obj[_.keys(lang_obj)[0]];
     }
 
     function setSelectedLanguage(lang) {
         selected_lang = lang;
     }
 
-    function setDefaultLanguage(lang) {
-        default_lang = lang;
-    }
-
     parseTranslateable.setSelectedLanguage = setSelectedLanguage;
-    parseTranslateable.setDefaultLanguage = setDefaultLanguage;
     parseTranslateable.getAvailableLanguages = getAvailableLanguages;
 
     return parseTranslateable;
