61c359c48d872860f48c5f298b5e62b293922294
Jonathan Wohl <jon@jonwohl.com>
2015-11-13 10:37:31 -0500

    WIP - updated eslint to include jest stuff

--- tests/js/example-test.js
+++ tests/js/example-test.js
@@ -1,6 +1,16 @@
 //TODO: deleteme!
-describe('a', function() {
-  it('is', function() {
-    expect(3).toBe(4);
-  });
+// jest.dontMock('../../dokomoforms/static/src/survey/js/components/Header');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+
+// const Header = require('../../dokomoforms/static/src/survey/js/components/Header');
+
+describe('Header', () => {
+
+    it('does something', () => {
+        expect(3).toEqual(3);
+    });
+
 });
