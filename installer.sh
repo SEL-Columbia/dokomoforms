af553f3a1dca7c1cc9b6773dcd12ec73bd6c1fff
Viktor Roytman <vr2262@columbia.edu>
2015-12-16 12:16:10 -0500

    Bump version

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-# Dokomo Forms installer for version 0.2.3
+# Dokomo Forms installer for version 0.2.4
 set -e
 
 # Do you have docker installed?
@@ -104,8 +104,8 @@ $SUDO openssl dhparam -out /etc/letsencrypt/live/$LETSENCRYPT_DIR/dhparam.pem 20
 printf "========================================\n"
 printf " Downloading configuration files        \n"
 printf "========================================\n"
-$CURL -O https://raw.githubusercontent.com/SEL-Columbia/dokomoforms/v0.2.3/docker-compose.yml
-$CURL -O https://raw.githubusercontent.com/SEL-Columbia/dokomoforms/v0.2.3/nginx.conf
+$CURL -O https://raw.githubusercontent.com/SEL-Columbia/dokomoforms/v0.2.4/docker-compose.yml
+$CURL -O https://raw.githubusercontent.com/SEL-Columbia/dokomoforms/v0.2.4/nginx.conf
 
 # Edit the configuration files
 printf "========================================\n"
