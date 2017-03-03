6679426a375cd076d220bb440abc2c3a5821d44d
Meredith Coulis <coulism@gmail.com>
2017-03-03 15:55:34 -0500

    denormalize before submitting

--- dokomoforms/static/src/admin/js/create-survey/Application.babel.js
+++ dokomoforms/static/src/admin/js/create-survey/Application.babel.js
@@ -1,74 +1,51 @@
-import React from 'react';
-import Survey from './components/Survey.babel.js';
+'use strict';
+
 import cookies from '../../../common/js/cookies';
 import $ from 'jquery';
-import utils from './utils.js';
+import Survey from './components/Survey.babel.js';
 import SubSurvey from './components/SubSurvey.babel.js';
-import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
-import {getNode, getSurvey, denormalize} from './redux/actions.babel.js';
-import {surveySelector, nodeSelector} from './redux/selectors.babel.js';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { orm } from './redux/models.babel.js';
+
 
 class Application extends React.Component {
 
     constructor(props) {
         super(props);
-
+        
         this.submitToDatabase = this.submitToDatabase.bind(this);
-        this.submitHandler = this.submitHandler.bind(this);
-
-        this.state = {
-            submitted: false,
-            languages: ['English']
-        }
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        if (this.state.submitted===true ||
-            this.props.currentSurveyId!=nextProps.currentSurveyId) {
-            console.log('top level app update');
-            return true;
-        }
-        return false;
-    }
-
-
-    submitHandler(){
-        this.setState({submitted: true}, function(){
-            console.log('submit handler')
-            this.props.denormalize();
-            this.submitToDatabase();
-        });
     }
 
 
     submitToDatabase() {
-        let survey = this.props.survey.denormalized;
-        console.log('submitting', survey)
+        console.log('submitting to database');
 
-        // this.setState({submitted: true}, function(){
-        //     console.log('submitting', survey)
-        //     survey.survey_type = 'public';
-        //     $.ajax({
-        //         type: "POST",
-        //         url: "/api/v0/surveys",
-        //         contentType: 'application/json',
-        //         processData: false,
-        //         data: JSON.stringify(denormalized),
-        //         headers: {
-        //           'X-XSRFToken': cookies.getCookie('_xsrf')
-        //         },
-        //         dataType: 'json',
-        //         success: function(response) {
-        //           console.log('success!!!');
-        //         },
-        //         error: function(response) {
-        //             console.log('error')
-        //           console.log(response.responseText);
-        //         }
-        //     })
+        const session = orm.session(this.props.state);
 
-        // })
+        const survey = session.Survey.withId(1001).denormalize();
+
+        console.log('survey', survey);
+        survey.survey_type = "public";
+
+        $.ajax({
+            type: "POST",
+            url: "/api/v0/surveys",
+            contentType: 'application/json',
+            processData: false,
+            data: JSON.stringify(survey),
+            headers: {
+              'X-XSRFToken': cookies.getCookie('_xsrf')
+            },
+            dataType: 'json',
+            success: function(response) {
+              console.log('success!!!');
+            },
+            error: function(response) {
+                console.log('error')
+              console.log(response.responseText);
+            }
+        });
     }
 
     render() {
@@ -76,23 +53,20 @@ class Application extends React.Component {
         return (
             <div>
                 <div>
-                    <button onClick={this.props.denormalize}>done</button>
-                </div>
-                <div>
                     {(this.props.currentSurveyId && this.props.currentSurveyId===1001) &&
                         <Survey
                             surveyId={this.props.currentSurveyId}
-                            submitToDatabase={this.submitHandler}
+                            submitToDatabase={this.submitToDatabase}
                         />
                     }
                 </div>
                 <div>
-                {(this.props.currentSurveyId && this.props.currentSurveyId!==1001) &&
+                    {(this.props.currentSurveyId && this.props.currentSurveyId!==1001) &&
                         <SubSurvey
                             surveyId={this.props.currentSurveyId}
                             languages={this.state.languages}
                         />
-                }
+                    }
                 </div>
             </div>
         );
@@ -102,13 +76,9 @@ class Application extends React.Component {
 function mapStateToProps(state){
     console.log('application mapstate', state)
     return {
-        survey: surveySelector(state),
-        currentSurveyId: state.currentSurveyId
-    }
+        currentSurveyId: state.currentSurveyId,
+        state: state.orm,
+    };
 }
 
-function matchDispatchToProps(dispatch){
-    return bindActionCreators({denormalize: denormalize, getNode: getNode, getSurvey: getSurvey}, dispatch)
-}
-
-export default connect(mapStateToProps, matchDispatchToProps)(Application);
+export default connect(mapStateToProps)(Application);
---
+++ dokomoforms/static/src/admin/js/create-survey/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import ReactDOM from 'react-dom';
+import Application from './Application.babel.js';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import { createReducer } from 'redux-orm';
+import bootstrap from './redux/bootstrap.babel.js';
+import { orm } from './redux/models.babel.js';
+import { currentSurveyIdReducer } from './redux/reducers.babel.js';
+
+const rootReducer = combineReducers({
+	orm: createReducer(orm),
+	currentSurveyId: currentSurveyIdReducer
+});
+
+const store = createStore(rootReducer, bootstrap(orm));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Application />
+  </Provider>, document.getElementById('app'));
