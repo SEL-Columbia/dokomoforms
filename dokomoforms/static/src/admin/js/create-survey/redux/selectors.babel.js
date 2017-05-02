32ed48fe81e2dc31098816183d9dedb1d1a797f3
Meredith Coulis <coulism@gmail.com>
2017-05-02 12:50:21 -0400

    added path selector

--- dokomoforms/static/src/admin/js/create-survey/redux/selectors.babel.js
+++ dokomoforms/static/src/admin/js/create-survey/redux/selectors.babel.js
@@ -1,3 +1,5 @@
+'use strict';
+
 import { createSelector } from 'reselect';
 import { createSelector as ormCreateSelector } from 'redux-orm';
 import { orm } from './models.babel.js';
@@ -6,45 +8,51 @@ import { orm } from './models.babel.js';
 const ormSelector = state => state.orm;
 
 
-const denormalizeSelector = createSelector(
-    ormSelector,
-    state => state.currentSurveyId,
-    ormCreateSelector(orm, (session, currentSurveyId) => {
-        console.log('Running survey selector', currentSurveyId);
-        const survey = session.Survey.withId(1001);
-        const denormalized = survey.denormalize();
-        return denormalized;
-    })
-);
-
+// const denormalizeSelector = createSelector(
+//     ormSelector,
+//     state => state.submitted,
+//     ormCreateSelector(orm, (session, submitted) => {
+//         console.log('Running denormalized selector', submitted);
+//         if (submitted) {
+//             console.log('denormalizing')
+//             return session.Survey.withId(1001).denormalize();
+//         }
+//         else return;
+//     });
+// );
 
 const surveySelector = createSelector(
 	ormSelector,
 	state => state.currentSurveyId,
 	ormCreateSelector(orm, (session, currentSurveyId) => {
     	console.log('Running survey selector', currentSurveyId);
-    	const survey = session.Survey.withId(currentSurveyId);
-        const obj = survey.ref;
-
+    	
+        const survey = session.Survey.withId(currentSurveyId);
         console.log('survey', survey)
         console.log('surveynodes', survey.nodes)
+        console.log('surveybuckets', survey.buckets)
 
-        let buckets = [];
-        let the_survey = {};
-        if (survey.buckets) buckets = survey.buckets.toRefArray();
-        if (buckets.length) {
-            the_survey = Object.assign({}, obj, {
+        let obj = survey.ref;
+
+        console.log('obj')
+
+        if (survey.buckets.toRefArray().length>0) {
+            obj = Object.assign({}, obj, {
                 buckets: survey.buckets.toRefArray()
-            })
-        }
+            });
+        };
+        // let buckets = survey.buckets.toRefArray();
+        // if (buckets.length) {
+        //     the_survey = Object.assign({}, obj, {
+        //         buckets: survey.buckets.toRefArray()
+        //     })
+        // }
 
         console.log('obj', obj)
-        console.log('nodes', nodes)
-		the_survey = Object.assign({}, the_survey, obj, {
-			nodes: survey.nodes.toRefArray()
-		})
 
-        return the_survey;
+		return Object.assign({}, obj, {
+			nodes: survey.nodes.toRefArray()
+		});
  	})
 );
 
@@ -59,6 +67,42 @@ const parentSurveySelector = createSelector(
     })
 );
 
+const parentQuestionTitleSelector = createSelector(
+    ormSelector,
+    state => state.currentSurveyId,
+    ormCreateSelector(orm, (session, currentSurveyId) => {
+        const parentNodeId = session.Survey.withId(currentSurveyId).node.id;
+        return session.Node.withId(parentNodeId).question.title;
+    })
+);
+
+const pathSelector = createSelector(
+    ormSelector,
+    state => state.currentSurveyId,
+    ormCreateSelector(orm, (session, currentSurveyId) => {
+
+        let survey = session.Survey.withId(currentSurveyId);
+
+        const path = [];
+
+        while (survey) {
+            if (!survey.node) break;
+            
+            const parentNode = survey.node;
+
+            const title = parentNode.question.title;
+            const buckets = parentNode.buckets || [];
+
+            path.push({title: title, buckets: buckets})
+            survey = parentNode.survey;
+        }
+
+        console.log('path', path);
+
+        return path;
+
+    })
+);
 
 const nodeSelector = createSelector(
     ormSelector,
@@ -75,69 +119,41 @@ const nodeSelector = createSelector(
         // });
         // const question = session.Node.withId(1).question;
         // console.log('question', question)
-        const nodes = session.Node.all();
-        let surveyNodes = nodes.filter(node => node.survey == currentSurveyId)
-        surveyNodes = surveyNodes.toModelArray();
-        console.log('surveyNodes', surveyNodes)
-        surveyNodes = surveyNodes.map(function(node){
-            console.log('ndee', session.Question.withId(node.id))
+        const nodes = session.Survey.withId(currentSurveyId).nodes;
+        console.log('nodes', nodes)
+        // const nodes = session.Node.all();
+        // let surveyNodes = nodes.filter(node => node.survey == currentSurveyId)
+        // // surveyNodes = surveyNodes.toModelArray();
+        // console.log('surveyNodes', surveyNodes)
+        let surveyNodes = nodes.toModelArray().map(function(node){
+            console.log('node', session.Question.withId(node.id))
             // const logic = node.logic ? node.logic.ref : {};
             const question = session.Question.withId(node.id).ref;
-            node = Object.assign({}, node.ref, {question: question});
-            let subArray = session.Survey.filter(survey => survey.node == node.id)
-            subArray = subArray.toRefArray();
-            console.log('arrayyyy', subArray)
-            if (subArray.length) return Object.assign({}, node, {sub_surveys: subArray})
-            console.log(node)
-            return node;
-        })
-        console.log(surveyNodes);
+            console.log('question', question)
+            let surveyNode = Object.assign({}, node.ref);
+
+            if (node.sub_surveys.toRefArray().length>0) {
+                surveyNode = Object.assign({}, surveyNode, {
+                    sub_surveys: node.sub_surveys.toRefArray()
+                });
+            }
+            // let surveys1 = session.Survey.all().toRefArray();
+            // console.log(surveys1)
+            // let subArray = session.Survey.all().filter(survey => survey.node == question.id)
+            // subArray = subArray.toRefArray();
+            // // console.log('arrayyyy', subArray)
+            // if (subArray.length) return Object.assign({}, node, {sub_surveys: subArray})
+            console.log(surveyNode)
+            return surveyNode;
+        });
         return surveyNodes;
-        // console.log(survey_nodes);
-
-        // let surveynodes1 = {};
-        // const surveynodes = survey_nodes.toModelArray().map(function(node){
-        //     console.log(node.sub_surveys)
-        //     if (node.sub_surveys) surveynodes1[node.id] = node.sub_surveys;
-        // })
-        // console.log(surveynodes1);
-        // return session.Node.all().toRefArray().filter(node => node.survey == currentSurveyId).map(node => {
-        //     const obj = Object.assign({}, node);
-        //     console.log('obj', obj)
-        //     const subs = node.sub_surveys.toRefArray();
-        //     if (subs.length) obj.sub_surveys = subs;
-
-        //     return obj
-        // })
-        // const obj = survey.ref;
-
-        // const testnodes = survey.nodes.toRefArray();
-        // console.log('obj', obj)
-        // console.log('nodes', testnodes)
-
-        // return Object.assign({}, obj, {
-        //     nodes: survey.nodes.toRefArray()
-        // })
-
-
-        // // let nodes = session.Node.all().filter(node => node.survey == currentSurveyId)
-        // nodes.toModelArray().map(function(node){
-        //     if (node.sub_surveys) {
-        //         console.log('not ref', sub)
-        //         return Object.assign(node, node.sub_surveys.toRefArray());
-        //     }
-        //     return node.ref;
-        // })
-        // let subObj = {};
-        // if (subsurveys) {
-        //     subObj = {sub_surveys: subsurveys}
-        // }
-        // return nodes.toRefArray();
     })
 );
 
 
-
-export {surveySelector, nodeSelector, parentSurveySelector, denormalizeSelector};
-
+export { surveySelector,
+        nodeSelector,
+        parentSurveySelector,
+        parentQuestionTitleSelector,
+        pathSelector };
 
